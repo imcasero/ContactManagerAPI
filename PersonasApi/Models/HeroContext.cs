2542a718b598604e9c5407bd12cced0da252b997
imcasero <diegocaserosmr@gmail.com>
2023-04-14 15:44:54 +0200

    v1.0
    This is the first version

---
+++ PersonasApi/Models/HeroContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace PersonasApi.Models
+{
+    public class HeroContext : DbContext
+    {
+        public HeroContext(DbContextOptions<HeroContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Hero> Heroes { get; set; }
+    }
+}
+
